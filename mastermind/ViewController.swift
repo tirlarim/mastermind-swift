2b1a108c46f7b86535465a76923fcaddf28be915
Stanislav Romanov <stasromanov801@gmail.com>
2024-06-02 10:53:02 +0200

    add indicators

--- mastermind/ViewController.swift
+++ mastermind/ViewController.swift
@@ -12,10 +12,11 @@ class ViewController: UIViewController {
     // Define the size of each peg
     let pegSize: CGFloat = 30.0
     let spacing: CGFloat = 10.0
+    let margin: CGFloat = 60.0
     
     // Calculate the total width and height of the game board
-    let boardWidth = CGFloat(columns) * (pegSize + spacing) - spacing
-    let boardHeight = CGFloat(rows) * (pegSize + spacing) - spacing
+    let boardWidth = CGFloat(columns) * (pegSize + spacing) - spacing + margin
+    let boardHeight = CGFloat(rows) * (pegSize + spacing) - spacing + margin
     
     // Create a UIView to represent the game board
     let gameBoard = UIView()
@@ -29,14 +30,31 @@ class ViewController: UIViewController {
     for row in 0..<rows {
       for col in 0..<columns {
         let peg = UIView()
-        let xPosition = CGFloat(col) * (pegSize + spacing)
-        let yPosition = CGFloat(row) * (pegSize + spacing)
+        let xPosition = CGFloat(col) * (pegSize + spacing) + margin/2.0
+        let yPosition = CGFloat(row) * (pegSize + spacing) + margin/2.0
         peg.frame = CGRect(x: xPosition, y: yPosition, width: pegSize, height: pegSize)
         peg.backgroundColor = UIColor.white
         peg.layer.borderWidth = 1.0
         peg.layer.borderColor = UIColor.black.cgColor
         peg.layer.cornerRadius = pegSize / 2
         gameBoard.addSubview(peg)
+      }
+      // at the end of line render indicators
+      let scalefactor = 4.0
+      let indicatorsMargin = CGVector(dx: CGFloat(0), dy: CGFloat(pegSize/2.0/scalefactor))
+      for indicatorsRow in 0..<2 {
+        for indicatorsCol in 0..<2 {
+          let peg = UIView()
+          let startPos = CGVector(dx: CGFloat(columns) * (pegSize + spacing) + margin/2.0, dy: CGFloat(row) * (pegSize + spacing) + margin/2.0)
+          let xPosition = startPos.dx + CGFloat(indicatorsRow) * spacing + indicatorsMargin.dx
+          let yPosition = startPos.dy + CGFloat(indicatorsCol) * spacing + indicatorsMargin.dy
+          peg.frame = CGRect(x: xPosition, y: yPosition, width: pegSize/scalefactor, height: pegSize/scalefactor)
+          peg.backgroundColor = UIColor.red
+          peg.layer.borderWidth = 1.0/scalefactor
+          peg.layer.borderColor = UIColor.black.cgColor
+          peg.layer.cornerRadius = pegSize / 2 / scalefactor
+          gameBoard.addSubview(peg)
+        }
       }
     }
   }
